bdf82977f25dd9223b03dbcc332d0ec01deaa4af
Anik <baust.cse.160201078@gmail.com>
2021-10-26 04:19:41 +0600

    chart added

--- src/Pages/AboutUs/AboutUs.js
+++ src/Pages/AboutUs/AboutUs.js
@@ -1,17 +1,44 @@
 import React from 'react';
+import Chart from 'react-google-charts';
 import aboutus from '../../Images/About us/unnamed.jpg';
 
 const AboutUs = () => {
     return (
-        <div className="aboutUs flex-container container w-100 ">
-                <div className="w-100 mt-5 text-success about" >
-                    <h1 className="text-primary">About Us</h1>
-                    <p>We provide the most comprehensive range of healthcare services in Victoria through our three hospital campuses, a large network of community programs and 15 statewide services.</p>
-                </div>
-                <div className=" w-100 mt-5">
-                    <img src={aboutus} alt="" />
-                </div>
+       <div>
+            <div className="aboutUs flex-container container w-100 ">
+            <div className="w-100 mt-5 text-success about" >
+                <h1 className="text-primary">About Us</h1>
+                <p>We provide the most comprehensive range of healthcare services in Victoria through our three hospital campuses, a large network of community programs and 15 statewide services.</p>
             </div>
+            <div className=" w-100 mt-5">
+                <img src={aboutus} alt="" />
+            </div>
+            
+        </div>
+       
+        <div className="text-center container w-100 ">
+        <Chart
+            width={'300px'}
+            height={'300px'}
+            chartType="PieChart"
+            loader={<div>Loading Chart</div>}
+            data={[
+                ['Task', 'Hours per Day'],
+                ['Work', 11],
+                ['Eat', 2],
+                ['Commute', 2],
+                ['Watch TV', 2],
+                ['Sleep', 7],
+            ]}
+            options={{
+                title: 'My Daily Activities',
+                // Just add this option
+                pieHole: 0.4,
+            }}
+            rootProps={{ 'data-testid': '3' }}
+        />
+    </div>
+    </div>
     );
 };
 
